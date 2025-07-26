88a17c4f6c6dff924a971299bb7f07df63d459d9
Henry <hi@henry.moe>
2025-07-25 22:37:17 -0400

    update readme and add db schema file

--- README.md
+++ README.md
@@ -4,6 +4,25 @@ Social media application built using Python, FastAPI, JavaScript, HTML/TailwindC
 ## Background
 Dot was created to address the overwhelming negativity that dominates much of modern social media. In contrast to platforms filled with outrage, toxicity, and algorithm-driven noise, Dot offers a quiet, kind space focused entirely on positivity. It's a place to share uplifting moments - a song stuck in your head, a small win, or just something that made you smile - with no algorithms, no trolls, and no drama. Dot aims to bring back a sense of connection, empathy, and joy to the online experience.
 
+## Screenshots
+<details>
+
+<summary>Click me!</summary>
+
+### Landing Page
+![Screenshot of landing page](demo/landing-page.png "Landing Page")
+
+### Feed
+![Screenshot of feed page](demo/feed.png "Feed Page")
+
+### User Profile
+![Screenshot of user profile page](demo/user-profile.png "User Profile Page")
+
+### Dashboard (zoomed out slightly for clarity)
+![Screenshot of dashboard page](demo/dashboard.png "Dashboard Page")
+
+</details>
+
 ## Installation
 ### Clone the repository
 Clone the Dot repository by running:
@@ -30,6 +49,19 @@ As per Dot's stated purpose of preventing toxicity on social media, Dot includes
 
 Due to file size and copyright limitations, no sentiment analysis model is included in this repository. Instead, it is recommended to download a pretrained/fine-tuned sentiment analysis model from e.g. [Hugging Face](https://huggingface.co/). After download, the model and its associated JSON files will need to be placed into a directory called `model`. This may also require modifying the [classifier code](https://github.com/hmont/dot/blob/main/adapters/classifier.py) to better suit the different model.
 
+### Setup PostgreSQL database
+#### Note: The included `db.sql` schema file has not been extensively tested. If you run into any issues, try creating the tables manually using the SQLAlchemy models as a guide.
+
+Import the database structure:
+
+```bash
+# make sure you're in the project root directory!
+cd dot
+
+psql -U <YOUR_USERNAME> -d <YOUR_DATABASE> -f ext/db.sql
+```
+
+
 ### Setup NGINX reverse proxy (for production environments)
 Dot does not natively use HTTPS/SSL as part of the application itself. As such, in production, it is highly recommended to use Dot behind a reverse proxy which allows for HTTPS (NGINX in this example).
 
@@ -44,7 +76,7 @@ cd dot
 
 cp ext/nginx.example nginx.conf
 ```
-You can then modify the configuration as needed. Exactly which configuration you use will ultimately depend on your system and needs, but generally, all you need to modify is the `server_name` (to reflect your domain), as well as the `ssl_certificate` and `ssl_certificate_key` to reflect the location of your SSL certificate and key.
+You can then modify the configuration as needed. Exactly which configuration you use will ultimately depend on your setup and needs, but generally, all you need to modify is the `server_name` (to reflect your domain), as well as the `ssl_certificate` and `ssl_certificate_key` to reflect the location of your SSL certificate and key.
 
 After editing the configuration, create a link in `sites-enabled`:
 ```bash
---
+++ ext/db.sql
@@ -0,0 +1,168 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 14.18 (Ubuntu 14.18-0ubuntu0.22.04.1)
+-- Dumped by pg_dump version 14.18 (Ubuntu 14.18-0ubuntu0.22.04.1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: posts; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.posts (
+    id integer NOT NULL,
+    poster integer NOT NULL,
+    created_at timestamp without time zone DEFAULT now() NOT NULL,
+    updated_at timestamp without time zone DEFAULT now() NOT NULL,
+    content text NOT NULL
+);
+
+
+--
+-- Name: posts_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.posts_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: posts_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.posts_id_seq OWNED BY public.posts.id;
+
+
+--
+-- Name: user_preferences; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.user_preferences (
+    user_id integer NOT NULL,
+    is_private boolean DEFAULT false NOT NULL
+);
+
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.users (
+    id integer NOT NULL,
+    username character varying(255) NOT NULL,
+    created_at timestamp without time zone DEFAULT now() NOT NULL,
+    updated_at timestamp without time zone DEFAULT now() NOT NULL,
+    password_bytes bytea NOT NULL,
+    avatar_url text,
+    bio character varying(255),
+    privs character varying(255),
+    display_name text DEFAULT ''::text NOT NULL
+);
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.users_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
+
+
+--
+-- Name: posts id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.posts ALTER COLUMN id SET DEFAULT nextval('public.posts_id_seq'::regclass);
+
+
+--
+-- Name: users id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- Name: posts posts_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.posts
+    ADD CONSTRAINT posts_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: users unique_username; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT unique_username UNIQUE (username);
+
+
+--
+-- Name: user_preferences user_preferences_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.user_preferences
+    ADD CONSTRAINT user_preferences_pkey PRIMARY KEY (user_id);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: posts posts_poster_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.posts
+    ADD CONSTRAINT posts_poster_fkey FOREIGN KEY (poster) REFERENCES public.users(id);
+
+
+--
+-- Name: user_preferences user_preferences_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.user_preferences
+    ADD CONSTRAINT user_preferences_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
